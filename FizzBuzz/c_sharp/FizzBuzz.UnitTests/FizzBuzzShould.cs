494774a566b2995ecf3957640c3d655a666e40e2
Michael Stenger <michael.stenger@outlook.de>
2022-05-12 18:18:40 +0200

    implementing fizzbuzz and tests

--- FizzBuzz/c_sharp/FizzBuzz.UnitTests/FizzBuzzShould.cs
+++ FizzBuzz/c_sharp/FizzBuzz.UnitTests/FizzBuzzShould.cs
@@ -1,7 +1,8 @@
 using System;
 using System.IO;
-using Xunit;
+using System.Linq;
 using FizzBuzz;
+using Xunit;
 
 namespace FizzBuzz.UnitTests
 {
@@ -15,6 +16,8 @@ namespace FizzBuzz.UnitTests
             Console.SetOut(_stringWriter);
         }
 
+        private static void RunFizzBuzz(params int[] values) => FizzBuzz.Run(values);
+
         private string GetConsoleOutput() => _stringWriter.ToString();
 
         [Theory]
@@ -24,12 +27,93 @@ namespace FizzBuzz.UnitTests
         public void PrintFizzForNumbersDivisibleByThree(int inputValue)
         {
             // Arrange
+            var expectedOutput = "Fizz\n";
 
             //Act
-            FizzBuzz.Run(new [] {inputValue});
+            RunFizzBuzz(inputValue);
 
             //Assert
-            Assert.Equal("Fizz\n", GetConsoleOutput());
+            Assert.Equal(expectedOutput, GetConsoleOutput());
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(20)]
+        public void PrintBuzzForNumbersDivisibleByFive(int inputValue)
+        {
+            // Arrange
+            var expectedOutput = "Buzz\n";
+
+            //Act
+            RunFizzBuzz(inputValue);
+
+            //Assert
+            Assert.Equal(expectedOutput, GetConsoleOutput());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(15)]
+        [InlineData(30)]
+        public void PrintFizzBuzzForNumbersDivisibleByThreeAndFive(int inputValue)
+        {
+            // Arrange
+            var expectedOutput = "FizzBuzz\n";
+
+            //Act
+            RunFizzBuzz(inputValue);
+
+            //Assert
+            Assert.Equal(expectedOutput, GetConsoleOutput());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        public void PrintNumberForNumbersNotDivisibleByThreeOrFive(int inputValue)
+        {
+            // Arrange
+            var expectedOutput = $"{inputValue}\n";
+
+            //Act
+            RunFizzBuzz(inputValue);
+
+            //Assert
+            Assert.Equal(expectedOutput, GetConsoleOutput());
+        }
+
+        [Fact]
+        public void PrintCorrectOutputForSequenceOfNumbers()
+        {
+            // Arrange
+            var input = Enumerable.Range(1, 16);
+            var expectedOutput = string.Join('\n', new[]
+            {
+                "1",
+                "2",
+                "Fizz",
+                "4",
+                "Buzz",
+                "Fizz",
+                "7",
+                "8",
+                "Fizz",
+                "Buzz",
+                "11",
+                "Fizz",
+                "13",
+                "14",
+                "FizzBuzz",
+                "16\n"
+            });
+
+            // Act
+            RunFizzBuzz(input.ToArray());
+
+            //Assert
+            Assert.Equal(expectedOutput, GetConsoleOutput());
         }
     }
 }
--- FizzBuzz/c_sharp/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/c_sharp/FizzBuzz/FizzBuzz.cs
@@ -7,9 +7,22 @@ namespace FizzBuzz
         public static void Run(IEnumerable<int> values){
             foreach (var value in values)
             {
-                if(value % 3 == 0)
+                var fizz = value % 3 == 0;
+                var buzz = value % 5 == 0;
+
+                if(fizz)
                 {
                     Console.Write("Fizz");
+                }
+
+                if(buzz)
+                {
+                    Console.Write("Buzz");
+                }
+
+                if(!(fizz || buzz))
+                {
+                    Console.Write(value);
                 }
 
                 Console.Write("\n");
