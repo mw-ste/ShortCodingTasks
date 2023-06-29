571fce7c657a5437e1f581ef5e68a2d66b1c82e8
Michael Stenger <michael.stenger@outlook.de>
2023-06-29 16:07:52 +0200

    wip anagram finder

--- FindAnagrams/c_sharp/FindAnagrams/AnagramFinder.cs
+++ FindAnagrams/c_sharp/FindAnagrams/AnagramFinder.cs
@@ -4,6 +4,22 @@ public static class AnagramFinder
 {
     public static List<List<string>> Find(string text)
     {
+        if (string.IsNullOrEmpty(text))
+        {
+            return new List<List<string>>();
+        }
+
+        var parts = text
+            .Split(' ')
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Select(x =>
+            {
+                var charArray = x.ToLower().ToCharArray();
+                Array.Sort(charArray);
+                return (Key: charArray, x);
+            });
+
+        Console.WriteLine(parts);
         throw new NotImplementedException();
     }
 }
