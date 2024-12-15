a3cf82e125ca6b562be82b00f67d281da30ffe48
Michael Stenger <michael.stenger@outlook.de>
2024-12-15 20:03:30 +0100

    adding code sample

--- ReverseWords/c_sharp/ReverseWords.UnitTests/WordReverserShould.cs
+++ ReverseWords/c_sharp/ReverseWords.UnitTests/WordReverserShould.cs
@@ -4,9 +4,75 @@ using Xunit;
 
 public class WordReverserShould
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("a", "a")]
+    [InlineData("ab", "ab")]
+    [InlineData("abc", "abc")]
+    public void NotReverseShortWords(string inputText, string expectedResult)
     {
+        //Arrange
 
+        //Act
+        var result = WordReverser.Reverse(inputText);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("abcd", "dcba")]
+    [InlineData("abcde", "edcba")]
+    public void ReverseLongWords(string inputText, string expectedResult)
+    {
+        //Arrange
+
+        //Act
+        var result = WordReverser.Reverse(inputText);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void ReverseLongWordsInSentense()
+    {
+        //Arrange
+        const string text = "abc abcd ab abcde";
+        const string expectedResult = "abc dcba ab edcba";
+
+        //Act
+        var result = WordReverser.Reverse(text);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Theory]
+    [InlineData("", "")]
+    [InlineData(" ", " ")]
+    [InlineData("    ", "    ")]
+    public void ReturnEmptyOrWhitespaceTextsAsIs(string inputText, string expectedResult)
+    {
+        //Arrange
+
+        //Act
+        var result = WordReverser.Reverse(inputText);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    public void PreserveWhitespacesInSentense()
+    {
+        //Arrange
+        const string text = "abc abcd  ab   abcde";
+        const string expectedResult = "abc dcba  ab   edcba";
+
+        //Act
+        var result = WordReverser.Reverse(text);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
     }
 }
--- ReverseWords/c_sharp/ReverseWords/WordReverser.cs
+++ ReverseWords/c_sharp/ReverseWords/WordReverser.cs
@@ -1,6 +1,21 @@
 ﻿namespace ReverseWords;
 
+using System.Linq;
+
 public class WordReverser
 {
+    const char delimiter = ' ';
+    const int maxSize = 3;
 
+    public static string Reverse(string text)
+    {
+        return string.Join(delimiter, text.Split(delimiter).Select(ReverseIfLong));
+    }
+
+    private static string ReverseIfLong(string text)
+    {
+        return text.Length > maxSize
+            ? string.Join("", text.Reverse())
+            : text;
+    }
 }
